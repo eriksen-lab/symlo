d508d7e6cedfc0e70146dd84e3757db84a7fa83f
Jonas Greiner <jongr@kemi.dtu.dk>
2025-07-09 10:33:12 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="SymLo",
+    version="0.1",
+    description="SymLo: Code for the Symmetrization of Localized Molecular Orbitals",
+    author="Jonas Greiner",
+    author_email="jongr@dtu.dk",
+    packages=["symlo"],
+    python_requires=">=3.6",
+)
