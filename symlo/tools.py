ee4c81fd3f745ac28fe98f7db8068b39bd574fb3
jonas-greiner <jongr@kemi.dtu.dk>
2025-07-17 09:56:07 +0200

    Fix for symmetry-invariant block detection.

--- symlo/tools.py
+++ symlo/tools.py
@@ -850,7 +850,7 @@ def get_symm_inv_blocks(
         min_block = blocks[min_block_idx]
 
         # stop if the lowest total contribution is above threshold
-        if min_val > (1 - 1 / (len(min_block) + 1)):
+        if min_val > (1 - 1 / (len(min_block) + 1)) or len(blocks) == 1:
             break
 
         # merge with block which has the highest single overlap with this block
